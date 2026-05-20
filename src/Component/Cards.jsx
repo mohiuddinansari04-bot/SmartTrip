66ccd7e83fdf5ac94fe0a364d64ce7676ef8f44c
Mohiuddin ansari <mohiuddinansari04@gmail.com>
2026-05-20 16:46:59 +0530

    forth commit

--- src/Component/Cards.jsx
+++ src/Component/Cards.jsx
@@ -9,39 +9,39 @@ function Cards() {
     },
     {
       id: 2,
-      title: "go to mountain",
+      title: "see the mountain",
       Image: "https://images.unsplash.com/photo-1501785888041-af3ef285b470",
       dose: "Explore beautiful mountains and nature views.",
     },
     {
       id: 3,
-      title: "go to city",
+      title: "visit the city",
       Image: "https://images.unsplash.com/photo-1494526585095-c41746248156",
       dose: "Discover amazing city life and modern architecture.",
     },
 
-     {
+    {
       id: 4,
-      title: "go to beach",
-      Image: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e",
-      dose: "Enjoy beautiful beaches and amazing sunsets in Goa.",
+      title: "explor the world",
+      Image: "https://images.unsplash.com/photo-1499856871958-5b9627545d1a?w=800",
+      dose: "Experience the beauty of Paris and the Eiffel Tower.",
     },
     {
-      id: 2,
-      title: "go to mountain",
-      Image: "https://images.unsplash.com/photo-1501785888041-af3ef285b470",
-      dose: "Explore beautiful mountains and nature views.",
+      id: 5,
+      title: "Explore Dubai",
+      Image: "https://images.unsplash.com/photo-1512453979798-5ea266f8880c?w=800",
+      dose: "Enjoy luxury lifestyle and amazing skyscrapers.",
     },
     {
-      id: 3,
-      title: "go to city",
-      Image: "https://images.unsplash.com/photo-1494526585095-c41746248156",
-      dose: "Discover amazing city life and modern architecture.",
-    },
-    
-   
+      id: 6,
+      title: "Visit Switzerland",
+      Image: "https://images.unsplash.com/photo-1506744038136-46273834b3fb?w=800",
+      dose: "See snowy mountains and peaceful lakes.",
+    }
+
+
     // space-y-2 flex flex-col lg:flex-row px-4 py-3 justify-around mt-5 mb-5
-    
+
   ]
 
   return (
@@ -50,7 +50,7 @@ function Cards() {
         <div className="max-w-sm rounded-2xl overflow-hidden shadow-lg bg-white hover:scale-105 transition-transform duration-300">
           {/* Card Image */}
           <img src={card.Image} alt={card.title}
-        className="w-full h-56 object-cover hover:scale-105 transition-transform duration-400" />
+            className="w-full h-56 object-cover hover:scale-105 transition-transform duration-400" />
 
           <div className="p-5">
             <h2 className="text-2xl font-bold  text-gray-800">{card.title}</h2>
