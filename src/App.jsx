8a681373f8a2ed5b644fb82667af876ea5e2a9c3
Mohiuddin ansari <mohiuddinansari04@gmail.com>
2026-05-22 20:20:57 +0530

    main

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ function App() {
   return (
     <>
       <Nav />
+      
       <Routes>
         <Route path ="/" element={<Home />} />
         <Route path ="/about" element={<About />} />
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import Nav from './Component/Nav.jsx'
 import Slider from './Component/Slider.jsx'
 import Siderbar from './Component/Siderbar.jsx'
 import Footer from './Component/Footer.jsx'
@@ -16,7 +15,7 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
-    {/* <Nav /> */}
+
     <Slider />
     <Siderbar />
     <Cards />
