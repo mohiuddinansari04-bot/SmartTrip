4a1a7f055b25a263e10890334e3c0c4d14f2afe4
Mohiuddin ansari <mohiuddinansari04@gmail.com>
2026-05-23 23:39:17 +0530

    commit

--- src/Hero/About.jsx
+++ src/Hero/About.jsx
@@ -15,14 +15,19 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 const About = () => {
-
   const nav = useNavigate();
 
   return (
     <div>
-      <h1 className="font-bold text-3xl flex justify-around text-">About us </h1>
-      <div>
-        
+      <h1 className="font-bold text-3xl flex justify-around text-">
+        About us
+      </h1>
+
+      {/*container */}
+      <div className=" max-w-3xl border-2 mt-5 mb-5">
+        <h1></h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis hic harum fugiat itaque dolor error expedita rerum asperiores numquam. Officiis asperiores quo magnam! Ex pariatur hic perspiciatis, veritatis expedita neque!</p>
+
       </div>
     </div>
   );
