3a2a5b8732e1a1a92e98ab823f33c243818fd6cb
Mohiuddin ansari <mohiuddinansari04@gmail.com>
2026-05-24 23:50:33 +0530

    commit

--- src/Component/Nav.jsx
+++ src/Component/Nav.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaRocket } from "react-icons/fa";
-import { AiOutlineLogin } from "react-icons/ai";
+// import { AiOutlineLogin } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import { CiMenuBurger } from "react-icons/ci";
 
@@ -16,7 +16,7 @@ function Nav() {
           </h1>
 
           {/* Nav Links */}
-          <ul className="lg:flex flex-col sm:flex-row hidden justify-center items-center gap-4 text-lg font-medium">
+          <ul className="lg:flex flex-col sm:flex-row hidden justify-center items-center gap-6 text-lg font-medium mr-" >
             <li className="hover:underline p-2 rounded cursor-pointer text-white">
               <Link to="/">Home</Link>
             </li>
@@ -35,10 +35,10 @@ function Nav() {
 
           {/* Login Button */}
 
-          <button className="flex items-center gap-2 text-white text-lg sm:text-xl px-4 py-2 rounded hover:bg-sky-500 ">
+          {/* <button className="flex items-center gap-2 text-white text-lg sm:text-xl px-4 py-2 rounded hover:bg-sky-500 ">
             <AiOutlineLogin />
             Login
-          </button>
+          </button> */}
         </nav>
       </div>
     </>
