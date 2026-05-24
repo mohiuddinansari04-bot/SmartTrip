2298471cc83d3ac1b9e3cd08814cf7eeaafd7ebf
Mohiuddin ansari <mohiuddinansari04@gmail.com>
2026-05-24 23:28:16 +0530

    commit

--- src/Hero/About.jsx
+++ src/Hero/About.jsx
@@ -1,16 +1,3 @@
-// import react from "react";
-// import { Navigate } from "react-router-dom";
-// const About =()=>{
-
-//   const Nav =Navigate();
-//   return (
-//     <div>
-//       <h1>About Us</h1>
-//       <p>This is a simple about page component.</p>
-//     </div>
-//   )
-// }
-// export default About;
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
